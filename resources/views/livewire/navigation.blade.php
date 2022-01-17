6ddf3c40554b2edd72de430096c35eb834037cc7
alberto <albertojcr96@gmail.com>
2022-01-17 12:11:07 +0100

    Fix icono hamburguer

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -3,7 +3,7 @@
         <div class="container-menu flex items-center h-16">
             <a :class="{'bg-opacity-100 text-orange-500': open}" x-on:click="show()" class="flex flex-col items-center justify-center px-4 bg-white bg-opacity-25 text-white cursor-pointer font-semibold h-full">
                 <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                    <path class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M412h16M4 18h16" />
+                    <path class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
                 </svg>
                 <span class="text-sm">Categorías</span>
             </a>
