667f8afd70ed5d6abb536400a71fb0c6c5d110e6
albertojcr <albertojcr96@gmail.com>
2022-01-19 18:55:12 +0100

    Vista de detalles de producto

--- resources/views/livewire/category-filter.blade.php
+++ resources/views/livewire/category-filter.blade.php
@@ -55,7 +55,7 @@
                                 </figure>
                                 <div class="py-4 px-6">
                                     <h1 class="text-lg font-semibold">
-                                        <a href="">
+                                        <a href="{{ route('products.show', $product) }}">
                                             {{ Str::limit($product->name, 20) }}
                                         </a>
                                     </h1>
@@ -92,9 +92,9 @@
                                         </div>
                                     </div>
                                     <div class="mt-auto mb-6">
-                                        <x-jet-danger-button>
+                                        <x-danger-link href="{{ route('products.show', $product) }}">
                                             Más información
-                                        </x-jet-danger-button>
+                                        </x-danger-link>
                                     </div>
                                 </div>
                             </article>
