a4c117b348feb66a6257a1a553ac50fa9c81f73f
albertojcr <albertojcr96@gmail.com>
2022-02-04 17:10:48 +0100

    22. Edición de productos

--- routes/admin.php
+++ routes/admin.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Livewire\Admin\CreateProduct;
+use App\Http\Livewire\Admin\EditProduct;
 use App\Http\Livewire\Admin\ShowProducts;
 use Illuminate\Support\Facades\Route;
 
@@ -11,3 +12,6 @@ Route::get('products/{product}/edit', function () {})->name('admin.products.edit
 Route::get('products/create', function () {})->name('admin.products.create');
 
 Route::get('products/create', CreateProduct::class)->name('admin.products.create');
+
+Route::get('products/{product}/edit', EditProduct::class)->name('admin.products.edit');
+
