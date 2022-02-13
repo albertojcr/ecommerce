5bd5ae120c2bc50c45792b6dfd6969efac5adc7c
alberto <albertojcr96@gmail.com>
2022-02-13 14:16:33 +0100

    Eliminado campo image

--- database/factories/SubcategoryFactory.php
+++ database/factories/SubcategoryFactory.php
@@ -15,7 +15,7 @@ class SubcategoryFactory extends Factory
     {
         return [
             'name' => $this->faker->sentence(2),
-            'image' => 'subcategories/' . $this->faker->image(storage_path('app/public/subcategories'), 640, 480, null, false),
+//            'image' => 'subcategories/' . $this->faker->image(storage_path('app/public/subcategories'), 640, 480, null, false),
             'slug' => $this->faker->slug(3),
             'color' => false,
             'size' => false
