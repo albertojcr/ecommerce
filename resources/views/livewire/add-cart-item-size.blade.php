0a4ba68e1f4bda44b2f7bc990574288e5ec83fe6
albertojcr <albertojcr96@gmail.com>
2022-02-15 17:25:54 +0100

    Fix - No se mostraba el stock correctamente

--- resources/views/livewire/add-cart-item-size.blade.php
+++ resources/views/livewire/add-cart-item-size.blade.php
@@ -20,7 +20,13 @@
     </div>
 
     <p dusk="available-stock" class="text-gray-700 my-4">
-        <span class="font-semibold text-lg">Stock disponible:</span> {{ $product->stock }}
+        <span class="font-semibold text-lg">Stock disponible:</span>
+
+        @if($quantity)
+            {{ $quantity }}
+        @else
+            {{ $product->stock }}
+        @endif
     </p>
 
     <div class="flex">
