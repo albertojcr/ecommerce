f73d25583d5e0f57f3ddf2fd691c151b1e22cc9d
albertojcr <albertojcr96@gmail.com>
2022-02-16 18:04:53 +0100

    Semana 4 - Tarea 8

---
+++ tests/Feature/Livewire/CreateProductsTest.php
@@ -0,0 +1,384 @@
+<?php
+
+namespace Tests\Feature\Livewire;
+
+use App\Http\Livewire\Admin\CreateProduct;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Livewire\Livewire;
+use Tests\TestCase;
+
+class CreateProductsTest extends TestCase
+{
+
+    /** @test */
+    public function it_creates_a_simple_product()
+    {
+        $category = $this->createCategory();
+        $subcategory = $this->createSubcategory($category->id);
+        $brand = $this->createBrand($category->id);
+
+        $user = $this->createAdminUser();
+
+        $this->actingAs($user);
+
+        Livewire::test(CreateProduct::class)
+            ->set('category_id', $category->id)
+            ->set('subcategory_id', $subcategory->id)
+            ->set('brand_id', $brand->id)
+            ->set('name', 'Nombre del producto')
+            ->set('slug', 'nombre-del-producto')
+            ->set('description', 'Descripción del producto')
+            ->set('price', 5)
+            ->set('quantity', 10)
+            ->call('save');
+
+        $this->assertDatabaseCount('products', 1);
+    }
+
+    /** @test */
+    public function it_creates_a_product_with_color()
+    {
+        $category = $this->createCategory();
+        $subcategory = $this->createSubcategory($category->id, true);
+        $brand = $this->createBrand($category->id);
+
+        $user = $this->createAdminUser();
+
+        $this->actingAs($user);
+
+        Livewire::test(CreateProduct::class)
+            ->set('category_id', $category->id)
+            ->set('subcategory_id', $subcategory->id)
+            ->set('brand_id', $brand->id)
+            ->set('name', 'Nombre del producto')
+            ->set('slug', 'nombre-del-producto')
+            ->set('description', 'Descripción del producto')
+            ->set('price', 5)
+            ->call('save');
+
+        $this->assertDatabaseCount('products', 1);
+    }
+
+    /** @test */
+    public function it_creates_a_product_with_color_and_size()
+    {
+        $category = $this->createCategory();
+        $subcategory = $this->createSubcategory($category->id, true, true);
+        $brand = $this->createBrand($category->id);
+
+        $user = $this->createAdminUser();
+
+        $this->actingAs($user);
+
+        Livewire::test(CreateProduct::class)
+            ->set('category_id', $category->id)
+            ->set('subcategory_id', $subcategory->id)
+            ->set('brand_id', $brand->id)
+            ->set('name', 'Nombre del producto')
+            ->set('slug', 'nombre-del-producto')
+            ->set('description', 'Descripción del producto')
+            ->set('price', 5)
+            ->call('save');
+
+        $this->assertDatabaseCount('products', 1);
+    }
+
+    /** @test  */
+    public function the_category_id_field_is_required()
+    {
+        $category = $this->createCategory();
+        $subcategory = $this->createSubcategory($category->id);
+        $brand = $this->createBrand($category->id);
+
+        $user = $this->createAdminUser();
+
+        $this->actingAs($user);
+
+        Livewire::test(CreateProduct::class)
+            ->set('category_id', '')
+            ->set('subcategory_id', $subcategory->id)
+            ->set('brand_id', $brand->id)
+            ->set('name', 'Nombre del producto')
+            ->set('slug', 'nombre-del-producto')
+            ->set('description', 'Descripción del producto')
+            ->set('price', 5)
+            ->set('quantity', 10)
+            ->call('save')
+            ->assertHasErrors(['category_id'=> 'required']);
+
+        $this->assertDatabaseCount('products', 0);
+    }
+
+    /** @test  */
+    public function the_subcategory_id_field_is_required()
+    {
+        $category = $this->createCategory();
+        $brand = $this->createBrand($category->id);
+
+        $user = $this->createAdminUser();
+
+        $this->actingAs($user);
+
+        Livewire::test(CreateProduct::class)
+            ->set('category_id', $category->id)
+            ->set('subcategory_id', '')
+            ->set('brand_id', $brand->id)
+            ->set('name', 'Nombre del producto')
+            ->set('slug', 'nombre-del-producto')
+            ->set('description', 'Descripción del producto')
+            ->set('price', 5)
+            ->set('quantity', 10)
+            ->call('save')
+            ->assertHasErrors(['subcategory_id'=> 'required']);
+
+        $this->assertDatabaseCount('products', 0);
+    }
+
+    /** @test */
+    public function the_name_field_is_required()
+    {
+        $category = $this->createCategory();
+        $subcategory = $this->createSubcategory($category->id);
+        $brand = $this->createBrand($category->id);
+
+        $user = $this->createAdminUser();
+
+        $this->actingAs($user);
+
+        Livewire::test(CreateProduct::class)
+            ->set('category_id', $category->id)
+            ->set('subcategory_id', $subcategory->id)
+            ->set('brand_id', $brand->id)
+            ->set('name', '')
+            ->set('slug', 'nombre-del-producto')
+            ->set('description', 'Descripción del producto')
+            ->set('price', 5)
+            ->set('quantity', 10)
+            ->call('save')
+            ->assertHasErrors(['name' => 'required']);
+
+        $this->assertDatabaseCount('products', 0);
+    }
+
+    /** @test */
+    public function the_slug_field_is_required()
+    {
+        $category = $this->createCategory();
+        $subcategory = $this->createSubcategory($category->id);
+        $brand = $this->createBrand($category->id);
+
+        $user = $this->createAdminUser();
+
+        $this->actingAs($user);
+
+        Livewire::test(CreateProduct::class)
+            ->set('category_id', $category->id)
+            ->set('subcategory_id', $subcategory->id)
+            ->set('brand_id', $brand->id)
+            ->set('name', 'Nombre del producto')
+            ->set('slug', '')
+            ->set('description', 'Descripción del producto')
+            ->set('price', 5)
+            ->set('quantity', 10)
+            ->call('save')
+            ->assertHasErrors(['slug' => 'required']);
+
+        $this->assertDatabaseCount('products', 0);
+    }
+
+    /** @test */
+    public function the_slug_field_is_unique()
+    {
+        $category = $this->createCategory();
+        $subcategory = $this->createSubcategory($category->id);
+        $brand = $this->createBrand($category->id);
+
+        $user = $this->createAdminUser();
+
+        $this->actingAs($user);
+
+        Livewire::test(CreateProduct::class)
+            ->set('category_id', $category->id)
+            ->set('subcategory_id', $subcategory->id)
+            ->set('brand_id', $brand->id)
+            ->set('name', 'Nombre del producto')
+            ->set('slug', 'nombre-del-producto')
+            ->set('description', 'Descripción del producto')
+            ->set('price', 5)
+            ->set('quantity', 10)
+            ->call('save');
+
+        $this->assertDatabaseCount('products', 1);
+
+        Livewire::test(CreateProduct::class)
+            ->set('category_id', $category->id)
+            ->set('subcategory_id', $subcategory->id)
+            ->set('brand_id', $brand->id)
+            ->set('name', 'Nombre del producto')
+            ->set('slug', 'nombre-del-producto')
+            ->set('description', 'Descripción del producto')
+            ->set('price', 5)
+            ->set('quantity', 10)
+            ->call('save')
+            ->assertHasErrors(['slug' => 'unique']);
+
+        $this->assertDatabaseCount('products', 1);
+    }
+
+    /** @test */
+    public function the_description_field_is_required()
+    {
+        $category = $this->createCategory();
+        $subcategory = $this->createSubcategory($category->id);
+        $brand = $this->createBrand($category->id);
+
+        $user = $this->createAdminUser();
+
+        $this->actingAs($user);
+
+        Livewire::test(CreateProduct::class)
+            ->set('category_id', $category->id)
+            ->set('subcategory_id', $subcategory->id)
+            ->set('brand_id', $brand->id)
+            ->set('name', 'Nombre del producto')
+            ->set('slug', 'nombre-del-producto')
+            ->set('description', '')
+            ->set('price', 5)
+            ->set('quantity', 10)
+            ->call('save')
+            ->assertHasErrors(['description' => 'required']);
+
+        $this->assertDatabaseCount('products', 0);
+    }
+
+    /** @test */
+    public function the_brand_id_field_is_required()
+    {
+        $category = $this->createCategory();
+        $subcategory = $this->createSubcategory($category->id);
+
+        $user = $this->createAdminUser();
+
+        $this->actingAs($user);
+
+        Livewire::test(CreateProduct::class)
+            ->set('category_id', $category->id)
+            ->set('subcategory_id', $subcategory->id)
+            ->set('brand_id', '')
+            ->set('name', 'Nombre del producto')
+            ->set('slug', 'nombre-del-producto')
+            ->set('description', 'Descripción del producto')
+            ->set('price', 5)
+            ->set('quantity', 10)
+            ->call('save')
+            ->assertHasErrors(['brand_id' => 'required']);
+
+        $this->assertDatabaseCount('products', 0);
+    }
+
+    /** @test */
+    public function the_price_field_is_required()
+    {
+        $category = $this->createCategory();
+        $subcategory = $this->createSubcategory($category->id);
+        $brand = $this->createBrand($category->id);
+
+        $user = $this->createAdminUser();
+
+        $this->actingAs($user);
+
+        Livewire::test(CreateProduct::class)
+            ->set('category_id', $category->id)
+            ->set('subcategory_id', $subcategory->id)
+            ->set('brand_id', $brand->id)
+            ->set('name', 'Nombre del producto')
+            ->set('slug', 'nombre-del-producto')
+            ->set('description', 'Descripción del producto')
+            ->set('price', '')
+            ->set('quantity', 10)
+            ->call('save')
+            ->assertHasErrors(['price' => 'required']);
+
+        $this->assertDatabaseCount('products', 0);
+    }
+
+    /** @test */
+    public function the_quantity_field_is_required_when_creating_a_simple_product()
+    {
+        $category = $this->createCategory();
+        $subcategory = $this->createSubcategory($category->id);
+        $brand = $this->createBrand($category->id);
+
+        $user = $this->createAdminUser();
+
+        $this->actingAs($user);
+
+        Livewire::test(CreateProduct::class)
+            ->set('category_id', $category->id)
+            ->set('subcategory_id', $subcategory->id)
+            ->set('brand_id', $brand->id)
+            ->set('name', 'Nombre del producto')
+            ->set('slug', 'nombre-del-producto')
+            ->set('description', 'Descripción del producto')
+            ->set('price', '5')
+            ->set('quantity', '')
+            ->call('save')
+            ->assertHasErrors(['quantity' => 'required']);
+
+        $this->assertDatabaseCount('products', 0);
+    }
+
+    /** @test */
+    public function the_quantity_field_is_optional_when_creating_a_product_with_color()
+    {
+        $category = $this->createCategory();
+        $subcategory = $this->createSubcategory($category->id, true);
+        $brand = $this->createBrand($category->id);
+
+        $user = $this->createAdminUser();
+
+        $this->actingAs($user);
+
+        Livewire::test(CreateProduct::class)
+            ->set('category_id', $category->id)
+            ->set('subcategory_id', $subcategory->id)
+            ->set('brand_id', $brand->id)
+            ->set('name', 'Nombre del producto')
+            ->set('slug', 'nombre-del-producto')
+            ->set('description', 'Descripción del producto')
+            ->set('price', '5')
+            ->set('quantity', '')
+            ->call('save')
+            ->assertHasNoErrors('quantity');
+
+        $this->assertDatabaseCount('products', 1);
+    }
+
+    /** @test */
+    public function the_quantity_field_is_optional_when_creating_a_product_with_color_and_size()
+    {
+        $category = $this->createCategory();
+        $subcategory = $this->createSubcategory($category->id, true, true);
+        $brand = $this->createBrand($category->id);
+
+        $user = $this->createAdminUser();
+
+        $this->actingAs($user);
+
+        Livewire::test(CreateProduct::class)
+            ->set('category_id', $category->id)
+            ->set('subcategory_id', $subcategory->id)
+            ->set('brand_id', $brand->id)
+            ->set('name', 'Nombre del producto')
+            ->set('slug', 'nombre-del-producto')
+            ->set('description', 'Descripción del producto')
+            ->set('price', '5')
+            ->set('quantity', '')
+            ->call('save')
+            ->assertHasNoErrors('quantity');
+
+        $this->assertDatabaseCount('products', 1);
+    }
+
+}
