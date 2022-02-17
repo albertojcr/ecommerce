5f64aa31a27f01d36d0bca157e7a55bc1b7a27a2
albertojcr <albertojcr96@gmail.com>
2022-02-17 20:46:52 +0100

    WIP Semana 4 - Tarea 9

---
+++ tests/Feature/Livewire/EditProductsTest.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace Tests\Feature\Livewire;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class EditProductsTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    public function it_updates_a_simple_product()
+    {
+
+    }
+
+    /** @test */
+    public function it_updates_a_product_with_color()
+    {
+
+    }
+    /** @test */
+    public function it_updates_a_product_with_color_and_size()
+    {
+
+    }
+
+    /** @test  */
+    public function the_category_id_field_is_required()
+    {
+
+    }
+
+    /** @test  */
+    public function the_subcategory_id_field_is_required()
+    {
+
+    }
+
+    /** @test  */
+    public function the_name_field_is_required()
+    {
+
+    }
+
+    /** @test  */
+    public function the_slug_field_is_required()
+    {
+
+    }
+
+    /** @test  */
+    public function the_slug_field_is_unique()
+    {
+
+    }
+
+    /** @test  */
+    public function the_description_field_is_required()
+    {
+
+    }
+
+    /** @test  */
+    public function the_brand_id_field_is_required()
+    {
+
+    }
+
+    /** @test  */
+    public function the_price_field_is_required()
+    {
+
+    }
+
+    /** @test  */
+    public function the_quantity_field_is_numeric()
+    {
+
+    }
+
+    /** @test  */
+    public function the_quantity_field_is_required_when_updating_a_simple_product()
+    {
+
+    }
+
+    /** @test  */
+    public function the_color_id_field_is_required_when_updating_a_product_with_color()
+    {
+
+    }
+
+    /** @test  */
+    public function the_color_quantity_field_is_required_when_updating_a_product_with_color()
+    {
+
+    }
+
+    /** @test  */
+    public function the_color_quantity_field_is_numeric_when_updating_a_product_with_color()
+    {
+
+    }
+
+    /** @test  */
+    public function the_size_name_field_is_required_when_updating_a_product_with_color_and_size()
+    {
+
+    }
+
+    /** @test  */
+    public function the_color_id_field_is_required_when_updating_a_product_with_color_and_size()
+    {
+
+    }
+
+    /** @test  */
+    public function the_color_quantity_field_is_required_when_updating_a_product_with_color_and_size()
+    {
+
+    }
+
+    /** @test  */
+    public function the_color_quantity_field_is_numeric_when_updating_a_product_with_color_and_size()
+    {
+
+    }
+}
