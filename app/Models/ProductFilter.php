55fac2677520dadadb1d5a2f53981d695ef3eafb
alberto <albertojcr96@gmail.com>
2022-02-25 13:21:52 +0100

    Tarea Final 2 - orderBySubcategory

--- app/Models/ProductFilter.php
+++ app/Models/ProductFilter.php
@@ -4,5 +4,10 @@ namespace App\Models;
 
 class ProductFilter extends QueryFilter
 {
-
+    public function orderBySubcategory($query)
+    {
+        return $query->orderBy(Subcategory::select('name')
+            ->whereColumn('subcategories.id', 'products.subcategory_id')
+        )->get();
+    }
 }
