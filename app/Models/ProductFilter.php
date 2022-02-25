39c041464f18e30d0ef42fd46d9771da810be952
alberto <albertojcr96@gmail.com>
2022-02-25 13:54:34 +0100

    Tarea Final 2 - orderByCategory

--- app/Models/ProductFilter.php
+++ app/Models/ProductFilter.php
@@ -2,12 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Support\Facades\DB;
+
 class ProductFilter extends QueryFilter
 {
+    public function orderByCategory($query)
+    {
+        return $query->orderBy(DB::table('categories AS c')
+            ->selectRaw('c.name')
+            ->join('subcategories', 'c.id', '=', 'subcategories.category_id')
+            ->whereColumn('subcategories.id', 'products.subcategory_id')
+        );
+    }
+
     public function orderBySubcategory($query)
     {
         return $query->orderBy(Subcategory::select('name')
             ->whereColumn('subcategories.id', 'products.subcategory_id')
-        )->get();
+        );
     }
+
 }
